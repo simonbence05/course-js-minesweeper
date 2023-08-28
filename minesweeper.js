7be0783daf2974355a6681984a9fcaddb7f482e8
simonbence05 <sbence94@gmail.com>
2023-08-28 14:46:50 +0200

    drawiamge fuggvény

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,13 @@
-console.log("hello, világ");
+const image = document.getElementById('hidden'); // a kép betöltése
+const canvas = document.getElementById('myCanvas'); // a canvas betöltése
+const c = canvas.getContext('2d'); // a canvas context betöltése
+
+const size = 50; // a kép mérete
+
+drawImage(0, 0); // a kép kirajzolása
+drawImage(100, 100);
+drawImage(123, 234);
+
+function drawImage(x, y) {
+  c.drawImage(image, x, y, size, size);
+}
