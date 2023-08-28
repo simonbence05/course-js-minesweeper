231faa7e642d09d81d84721f62fe2542c2b5f860
simonbence05 <sbence94@gmail.com>
2023-08-28 22:50:37 +0200

    Tömbök

--- minesweeper.js
+++ minesweeper.js
@@ -3,10 +3,24 @@ const canvas = document.getElementById('myCanvas'); // a canvas betöltése
 const c = canvas.getContext('2d'); // a canvas context betöltése
 
 const size = 50; // a kép mérete
+const columns = canvas.width / size; // oszlopok száma
+const rows = canvas.height / size; // sorok száma
 
-drawImage(0, 0); // a kép kirajzolása
-drawImage(100, 100);
-drawImage(123, 234);
+let map = [
+  [9, 8, 0, 1, 1, 1, 0, 0],
+  [0, 0, 1, 2, 3]
+];
+
+console.log(map);
+drawMap();
+
+function drawMap () {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      drawImage(i * size, j * size);
+    }
+  }
+}
 
 function drawImage(x, y) {
   c.drawImage(image, x, y, size, size);
